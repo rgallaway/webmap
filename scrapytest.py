a7cf6b11069fb5256ee716a4f43052301ef336e5
anderson-ryan <anderson.ryan473@gmail.com>
2018-09-08 13:24:14 -0400

    Prototype creation of JSON

--- scrapytest.py
+++ scrapytest.py
@@ -1,18 +1,34 @@
 import scrapy
 import Queue
-import sys
+import json
+
+# this is hackathon code; it will not be well documented or put together. please forgive me
 
 from scrapy.crawler import CrawlerProcess
 
-class Response():
-    def __init__(self, name, children, edge):
+class Node():
+    def __init__(self, name, edges):
         self.name = name
-        self.children = children.toList()
+        self.edges = edges
+
+    def getJson(self):
+        return json.dumps({"name": self.name, "edges": self.edges})
 
 class Edge():
-    def __init__(self, type, width):
+    def __init__(self, weights,  node):
+        self.weights = weights
+        self.node = node
+
+    def getJson(self):
+        return json.dumps({"weights": self.weights, "node": self.node})
+
+class Weight():
+    def __init__(self, type, value):
         self.type = type
-        self.width = width
+        self.value = value
+
+    def getJson(self):
+        return json.dumps({"type": self.type, "value": self.value})
 
 class Runner():
     def __init__(self, startUrl, nodeLimit):
@@ -38,6 +54,8 @@ class LinksSpider(scrapy.Spider):
     seen = Queue.Queue()  # all nodes we have seen
     expanded = set()  # set of expanded nodes
     children = {}  # dictionary of parents to set of children
+
+
 
     seen.put(start_urls[0])
 
@@ -83,6 +101,22 @@ class LinksSpider(scrapy.Spider):
         for x in self.children:
             print x
             print(self.children[x])
+
+        root = self.start_urls[0]
+        for node in self.children[root]:
+
+    def createStructure(self, root):
+        if not len(self.children[root]):
+            node = Node(root, [])
+            return node.getJson()
+        else:
+            edges = []
+            for child in self.children[root]:
+                edges.append(Edge([], self.createStructure(child)))
+            node = Node(root, edges)
+            print node.getJson()
+            return node.getJson()
+
 
     def cleanString(self, string):
         string = string.replace('https://', '')
