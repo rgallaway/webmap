84759dbe7a355e61bbfe1934a0972227d8c3cf32
Ryan Gallaway <rgallaway@gatech.edu>
2018-09-07 20:52:16 -0400

    added map endpoint with params

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import constants
 from flask import Flask
+from flask import request
+from flask import send_file
 import json
 app = Flask(__name__)
 
@@ -11,6 +13,18 @@ def test_endpoint():
         mimetype='application/json'
     )
     return response
+
+@app.route('/map')
+def do_map():
+    url = request.args.get('url', type=str)
+    depth = request.args.get('depth', default=50, type=int)
+    # Pass URL and depth to Ryan's scraper engine
+    return str.format('Map endpoint: {}, depth: {}', url, depth)
+
+@app.route('/')
+def index():
+    # This will host Sam's React frontend page
+    return send_file('static/index.html')
 
 if __name__ == '__main__':
     app.run()
