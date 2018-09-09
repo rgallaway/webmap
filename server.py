a051cb1c6aa23bd260c7616fd08a532e67492a62
Ryan Gallaway <rgallaway@gatech.edu>
2018-09-08 20:53:59 -0400

    map endpoint takes rand as optional parameter (0 or 1)

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import request
 from flask import send_file
 from flask import Response
+from search import search
 import json
 import subprocess
 app = Flask(__name__)
@@ -18,28 +19,27 @@ def test_endpoint():
 @app.route('/map')
 def do_map():
     url = request.args.get('url', type=str)
-    depth = request.args.get('limit', type=str)
+    depth = request.args.get('limit', default='100', type=str)
+    rand = request.args.get('rand', default='0', type=str)
     resp = Response()
     resp.headers['Access-Control-Allow-Origin'] = '*'
     resp.mimetype = 'application/json'
     # Pass URL and depth to Ryan's scraper engine
-    output = subprocess.check_output(['python', '-m', 'scrapy', 'runspider', '-a', 'start=' + url, '-a', 'edgeLimit=' + depth, 'scrapytest.py'])
+    output = subprocess.check_output(['python', '-m', 'scrapy', 'runspider', '-a', 'start=' + url, '-a', 'edgeLimit=' + depth, '-a', 'rand=' + rand, 'scrapytest.py'])
     with open("data.json", "r") as datafile:
         resp.set_data(datafile.read())
     return resp
 
 @app.route('/search')
-def search():
+def run_search():
     source_url = request.args.get('source', type=str)
     dest_url = request.args.get('dest', type=str)
     resp = Response()
     resp.headers['Access-Control-Allow-Origin'] = '*'
-    resp.mimetime = 'application/json'
+    resp.mimetype = 'application/json'
     # Pass source & dest URLs to search function
     # Will return JSON of path between nodes
-    output = {}
-    output["source"] = source_url
-    output["dest"] = dest_url
+    output = search(source_url, dest_url)
     resp.set_data(json.dumps(output))
     return resp
 
